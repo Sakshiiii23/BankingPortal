be32e0514cca02a6223bd3c02e6011dd13348fa3
Sakshi <66660153+Sakshiiii23@users.noreply.github.com>
2021-05-10 14:57:57 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,57 +1,70 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(express.static("public"));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
-
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const inshorts = require("./DataFetcher");
+const axios = require("axios");
+// const NewsAPI = require("newsapi");
+// const newsapi = new NewsAPI("bc1fb1c1c9f34f6ba5489540f6c8eb9f");
 mongoose.connect("mongodb://localhost:27017/mydb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-var db = mongoose.connection;
-
-db.on("error", () => console.log("Error in Connecting to Database"));
-db.once("open", () => console.log("Connected to Database"));
-
+const schema = new mongoose.Schema({
+  username: String,
+  email: String,
+  pno: Number,
+  password: String,
+});
+const user = mongoose.model("user", schema);
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.post("/sign_up", (req, res) => {
-  var name = req.body.username;
-  var email = req.body.email;
-  var phno = req.body.pno;
-  var password = req.body.password;
-
-  var data = {
-    name: name,
-    email: email,
-    phno: phno,
-    password: password,
-  };
-
-  db.collection("users").insertOne(data, (err, collection) => {
+  user.exists({ email: req.body.email }, (err, result) => {
     if (err) {
-      throw err;
+      console.log(err);
+      return;
+    } else {
+      if (result) {
+        res.send(false);
+        console.log("Not Added");
+      } else {
+        const newUser = new user({
+          username: req.body.username,
+          email: req.body.email,
+          pno: req.body.pno,
+          password: req.body.password,
+        });
+        newUser.save();
+        res.send(true);
+        console.log("Added");
+      }
     }
-    console.log("Record Inserted Successfully");
   });
-
-  return res.redirect("index.html");
 });
 
 app
-  .get("/", (req, res) => {
-    res.set({
-      "Allow-access-Allow-Origin": "*",
+  .post("/index", (req, res) => {
+    var options = {
+      lang: "en",
+      category: "entertainment",
+      numOfResults: 25,
+      click: true,
+    };
+    inshorts.get(options, function (result) {
+      res.send(result);
     });
-    return res.redirect("register.html");
+    // var date = new Date();
+    // async function submit() {
+    //   try {
+    //     const response = await axios.get(
+    //       `https://newsapi.org/v2/everything?from=${date}&domains=bbc.co.uk,techcrunch.com,engadget.com&page=5&apiKey=bc1fb1c1c9f34f6ba5489540f6c8eb9f`
+    //     );
+    //     res.send(response.data);
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
+    // }
   })
   .listen(3000);
-
-console.log("Listening on PORT 3000");
