0477f31fe92c970fadce96116432b1dd564ae291
Sakshi <66660153+Sakshiiii23@users.noreply.github.com>
2021-05-12 20:03:23 +0530

    updated registration login pages added features

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-//jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const inshorts = require("./DataFetcher");
 const axios = require("axios");
-// const NewsAPI = require("newsapi");
-// const newsapi = new NewsAPI("bc1fb1c1c9f34f6ba5489540f6c8eb9f");
+
 mongoose.connect("mongodb://localhost:27017/mydb", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,8 +16,22 @@ const schema = new mongoose.Schema({
 });
 const user = mongoose.model("user", schema);
 var app = express();
+
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.get("/", function (req, res) {
+  return res.render("index");
+});
+
+app.get("/login", function (req, res) {
+  return res.render("login");
+});
+
+app.get("/sign_up", function (req, res) {
+  return res.render("register");
+});
 app.post("/sign_up", (req, res) => {
   user.exists({ email: req.body.email }, (err, result) => {
     if (err) {
@@ -43,8 +55,7 @@ app.post("/sign_up", (req, res) => {
     }
   });
 });
-
-app.post("/Login", (req, res) => {
+app.post("/login", (req, res) => {
   user.exists({ email: req.body.email }, (err, result) => {
     if (err) {
       console.log(err);
@@ -55,36 +66,25 @@ app.post("/Login", (req, res) => {
           console.log(err);
         } else {
           res.send(req.body.password === resultFind.password);
+          console.log("You logged In");
         }
       });
     }
   });
 });
 
-app
-  .get("/", (req, res) => {
-    var options = {
-      lang: "en",
-      category: "entertainment",
-      numOfResults: 25,
-      click: true,
-    };
-    res.set({
-      "Allow-access-Allow-Origin": "*",
-    });
-    inshorts.get(options, function (result) {
-      res.send(result);
-    });
-    // var date = new Date();
-    // async function submit() {
-    //   try {
-    //     const response = await axios.get(
-    //       `https://newsapi.org/v2/everything?from=${date}&domains=bbc.co.uk,techcrunch.com,engadget.com&page=5&apiKey=bc1fb1c1c9f34f6ba5489540f6c8eb9f`
-    //     );
-    //     res.send(response.data);
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
-    // }
-  })
-  .listen(3000);
+app.post("/index", (req, res) => {
+  var options = {
+    lang: "en",
+    category: "entertainment",
+    numOfResults: 25,
+    click: true,
+  };
+  inshorts.get(options, function (result) {
+    res.send(result);
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is start");
+});
